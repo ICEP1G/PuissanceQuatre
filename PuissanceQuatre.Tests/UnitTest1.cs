e3ed636f460cbfe857a92471bd31ac1b5f26270d
Thomas KIEFFER <thomaskieffer6@gmail.com>
2024-12-20 13:52:41 +0100

    Ajoutez des fichiers projet.

---
+++ PuissanceQuatre.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PuissanceQuatre.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PuissanceQuatre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PuissanceQuatre
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
